27f06788051a11375ee56e0a2db21e2fb0bf5566
Yusuf <yusuf.oeztemiz@student.fh-kiel.de>
2024-12-06 11:19:06 +0100

    init commit

---
+++ src/main/kotlin/GUI.kt
@@ -0,0 +1,54 @@
+import de.fhkiel.rob.legoosctester.gui.Incoming
+import de.fhkiel.rob.legoosctester.gui.Outgoing
+import de.fhkiel.rob.legoosctester.osc.OSCSender
+import java.awt.Dimension
+import java.awt.GridLayout
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JFrame.EXIT_ON_CLOSE
+
+import javax.swing.JPanel
+
+class GUI: JFrame() {
+
+    init {
+        title = "ButtonKram"
+        minimumSize =  Dimension(400, 400)
+        defaultCloseOperation = EXIT_ON_CLOSE
+
+        layout = GridLayout(3, 3)
+        add(JPanel())
+        val forward = JButton("A")
+        forward.addActionListener {
+            OSCSender("192.168.178.255", 9001).send("/mazer/motor/ab/angle", 0)
+            OSCSender("192.168.178.255", 9001).send("/mazer/motor/ab/run/target", 100, 360)
+        }
+        add(forward)
+
+        add(JPanel())
+        val left = JButton("<")
+        left.addActionListener {
+            OSCSender("192.168.178.255", 9001).send("/mazer/motor/ab/angle", 0)
+            OSCSender("192.168.178.255", 9001).send("/mazer/motor/ab/multirun/target", 100, 200, -200)
+        }
+        add(left)
+        add(JPanel())
+        val right = JButton(">")
+        right.addActionListener {
+            OSCSender("192.168.178.255", 9001).send("/mazer/motor/ab/angle", 0)
+            OSCSender("192.168.178.255", 9001).send("/mazer/motor/ab/multirun/target", 100, -200, 200)
+        }
+        add(right)
+        add(JPanel())
+
+        val back = JButton("V")
+        back.addActionListener {
+            OSCSender("192.168.178.255", 9001).send("/mazer/motor/ab/angle", 0)
+            OSCSender("192.168.178.255", 9001).send("/mazer/motor/ab/run/target", -100, -360)
+        }
+        add(back)
+
+        isVisible = true
+    }
+
+}
