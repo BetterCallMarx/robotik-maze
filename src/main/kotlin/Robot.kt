8dae43ce82902a68370c0b9c98f3efa8edbf3690
Yusuf <yusuf.oeztemiz@student.fh-kiel.de>
2024-12-12 12:20:56 +0100

    TODO und neue files

--- src/main/kotlin/Robot.kt
+++ src/main/kotlin/Robot.kt
@@ -1,12 +1,31 @@
 import de.fhkiel.rob.legoosctester.osc.OSCSender
 import de.fhkiel.rob.legoosctester.osc.OSCReceiver
-
+/*
+TODO: Alle sensoren und motoren ab roboter fertig bringen
+TODO: FUnktionen zu Bewegung und Erfassung von Sensordaten
+TODO: Einfache Graph Bildung mittels odometrischen daten
+TODO: Vielleicht homogene kooridaten in betracht ziehen
+TODO: Je mehr informationen desto mehr certainty, eventuell für lokalisierung odometrische und entfernungssensor
+TODO: Beim zurückfahren least squares slam verwenden um error zwischen poses zu minimieren
+ */
 class Robot {
     //Direction Robot is facing
     lateinit var direction: Direction
 
     fun turnHead(){
         //TODO
+    }
+
+    fun localization(){
+
+    }
+
+    fun taskPlanning(){
+
+    }
+
+    fun pathPlanning(){
+
     }
 
 
